5b119596079d91a416adc57e839eaed995c87dff
Daniel Felipe <daniell.felipe.2017@gmail.com>
2024-05-04 08:42:34 -0300

    chore: making some adjustments on main file

--- main.py
+++ main.py
@@ -2,11 +2,22 @@ from core.taxa import Taxa
 from core.cep import Cep
 import argparse
 
+
 def parse_args():
     parser = argparse.ArgumentParser(description='BR Tools')
-    parser.add_argument('--taxas', '-t', action='store_true', help='Mostra as taxas de juros atuais')
-    parser.add_argument('--cep', '-c', help='Busca informações de um CEP')
+    parser.add_argument(
+        '--taxas',
+        '-t',
+        action='store_true',
+        help='Mostra as taxas de juros atuais'
+    )
+    parser.add_argument(
+        '--cep',
+        '-c',
+        help='Busca informações de um CEP'
+    )
     return parser.parse_args()
+
 
 def main():
     args = parse_args()
@@ -19,6 +30,6 @@ def main():
         cep = Cep(args.cep)
         cep.show()
 
+
 if __name__ == '__main__':
     main()
-
